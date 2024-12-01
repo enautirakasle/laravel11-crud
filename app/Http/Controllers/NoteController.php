9b0e910c9549024f5636590a3e06b3b8c7e978f1
Eñaut AA <enaut.irakasle@gmail.com>
2024-12-01 19:14:11 +0100

    Tipas las respuestas de NoteController

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -4,25 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Note;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class NoteController extends Controller
 {
-    public function index(){
+    public function index(): View
+    {
         $notes = Note::all();
         return view('note.index', compact('notes'));
     }
 
-    public function create(){
+    public function create(): View
+    {
         return view('note.create');
     }
 
-    public function store(Request $request){
+    public function store(Request $request): RedirectResponse
+    {
 
         //una forma de guardar
-        $note = new Note;
-        $note->title = $request->title;
-        $note->description = $request->description;
-        $note->save();
+        // $note = new Note;
+        // $note->title = $request->title;
+        // $note->description = $request->description;
+        // $note->save();
 
         // otra forma
         // Note::create([
@@ -31,7 +36,7 @@ class NoteController extends Controller
         // ]);
 
         // otra
-        // Note::create($request->all());
+        Note::create($request->all());
 
         return redirect()->route('note.index');
     }
@@ -41,12 +46,14 @@ class NoteController extends Controller
         laravel ya ha hecho el Note::find($note)
         y nos pasa la nota
     */
-    public function edit(Note $note){
+    public function edit(Note $note): View
+    {
 
         return view('note.edit', compact('note'));
     }
 
-    public function update(Request $request, Note $note ){
+    public function update(Request $request, Note $note ): RedirectResponse
+    {
         // $note = Note::find($note);
         // $note->title = $request->title;
         // $note->description = $request->description;
@@ -56,14 +63,16 @@ class NoteController extends Controller
         return redirect()->route('note.index');
     }
 
-    public function show(Note $note){
+    public function show(Note $note): View
+    {
         return view('note.show', compact('note'));
     }
 
    // public function destroy(Request $request, Note $note){
    //seria como arriba pero como no se utiliza $request se puede quitar
 
-    public function destroy(Note $note){
+    public function destroy(Note $note): RedirectResponse
+    {
         $note->delete();
         return redirect()->route('note.index');
     }
