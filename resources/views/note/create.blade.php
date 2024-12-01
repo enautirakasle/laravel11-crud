132fca4d21d1bc5309a6659c6e1465f9e81f7483
Eñaut AA <enaut.irakasle@gmail.com>
2024-12-01 22:40:25 +0100

    mensajes de validacion

--- resources/views/note/create.blade.php
+++ resources/views/note/create.blade.php
@@ -8,11 +8,17 @@
     <div>
         <label for="title">Título</label>
         <input type="text" name="title" id="title"/>
+        @error('title')
+        <p style="color: red">{{ $message }}</p>
+        @enderror
     </div>
 
     <div>
         <label for="description">Descripción</label>
         <input type="text" name="description" id="description"/>
+        @error('description')
+        <p style="color: red">{{ $message }}</p>
+        @enderror
     </div>
 
     <input type="submit" value="Create"/>
