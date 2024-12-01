a8444951dfa8ac36576b35fad08980f7c8bb8b18
Eñaut AA <enaut.irakasle@gmail.com>
2024-12-01 22:15:22 +0100

    validazioa eta errore mezuak

--- resources/views/note/create.blade.php
+++ resources/views/note/create.blade.php
@@ -8,11 +8,17 @@
     <div>
         <label for="title">Título</label>
         <input type="text" name="title" id="title"/>
+        @error('title')
+            <p style="color: crimson">Error en el titulo</p>
+        @enderror
     </div>
 
     <div>
         <label for="description">Descripción</label>
         <input type="text" name="description" id="description"/>
+        @error('description')
+        <p style="color: crimson">Error en la descripcion</p>            
+        @enderror
     </div>
 
     <input type="submit" value="Create"/>
