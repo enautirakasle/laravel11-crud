5e9a0645a18d2fab21cc20e6cce8634fef0efd0f
Eñaut Agirre Arrizabalaga <enaut.irakasle@gmail.com>
2024-12-02 16:53:29 +0000

    cambio para que las rutas funcione en codespaces

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +21,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        URL::forceRootUrl(config('app.url'));
+
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
