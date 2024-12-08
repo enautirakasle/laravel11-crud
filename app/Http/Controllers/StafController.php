3b1383e517f66e2613a1a8cffe20568ef65207b9
Eñaut AA <enaut.irakasle@gmail.com>
2024-12-08 21:59:10 +0100

    sail artisan make:model Staf -crRm

---
+++ app/Http/Controllers/StafController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreStafRequest;
+use App\Http\Requests\UpdateStafRequest;
+use App\Models\Staf;
+
+class StafController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreStafRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Staf $staf)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Staf $staf)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateStafRequest $request, Staf $staf)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Staf $staf)
+    {
+        //
+    }
+}
