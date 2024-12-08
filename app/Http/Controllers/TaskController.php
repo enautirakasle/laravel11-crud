160a7d78be3a55680ff1448b71eaa3cdb961cb19
Eñaut AA <enaut.irakasle@gmail.com>
2024-12-08 14:47:32 +0100

    validazio gabe ok

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,32 +4,38 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
+use Illuminate\Http\Request;
 use App\Models\Task;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
 
 class TaskController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): View
     {
-        //
+        $tasks = Task::all();
+        return view('task.index', compact('tasks'));
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): View
     {
-        //
+        return view('task.create');
     }
 
     /**
      * Store a newly created resource in storage.
+     * StoreTaskRequest
      */
-    public function store(StoreTaskRequest $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+        Task::create($request->all());
+        return redirect()->route('task.index');
     }
 
     /**
@@ -45,15 +51,17 @@ class TaskController extends Controller
      */
     public function edit(Task $task)
     {
-        //
+        return view('task.edit', compact('task'));
     }
 
     /**
      * Update the specified resource in storage.
+     * UpdateTaskRequest
      */
-    public function update(UpdateTaskRequest $request, Task $task)
+    public function update(Request $request, Task $task)
     {
-        //
+        $task->update($request->all());
+        return redirect()->route('task.index');
     }
 
     /**
@@ -61,6 +69,7 @@ class TaskController extends Controller
      */
     public function destroy(Task $task)
     {
-        //
+        $task->delete();
+        return redirect()->route('task.index');
     }
 }
