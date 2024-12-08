4e6a3ed004f78223d587a61d44267e46328721e0
Eñaut AA <enaut.irakasle@gmail.com>
2024-12-08 15:21:33 +0100

    errore mezuak ok

--- resources/views/note/create.blade.php
+++ resources/views/note/create.blade.php
@@ -8,16 +8,12 @@
     <div>
         <label for="title">Título</label>
         <input type="text" name="title" id="title"/>
-        @error('title')
-            <p style="color: crimson">Error en el titulo</p>
-        @enderror
     </div>
 
     <div>
         <label for="description">Descripción</label>
         <input type="text" name="description" id="description"/>
-        @error('description')
-        <p style="color: crimson">Error en la descripcion</p>                </div>
+    </div>
 
     <input type="submit" value="Create"/>
 </form>
--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,12 @@ Route::put('/note/update/{note}', [NoteController::class, 'update'])->name('note
 Route::get('/note/show/{note}', [NoteController::class, 'show'])->name('note.show');
 Route::delete('/note/destroy/{note}', [NoteController::class, 'destroy'])->name('note.destroy');
 
+// Route::get('/task', [taskController::class, 'index'])->name('task.index');
+// Route::get('/task/create', [taskController::class, 'create'])->name('task.create');
+// Route::post('/task/store', [taskController::class, 'store'])->name('task.store');
+// Route::get('/task/edit/{task}', [taskController::class, 'edit'])->name('task.edit');
+// Route::put('/task/update/{task}', [taskController::class, 'update'])->name('task.update');
+// Route::get('/task/show/{task}', [taskController::class, 'show'])->name('task.show');
+// Route::delete('/task/destroy/{task}', [taskController::class, 'destroy'])->name('task.destroy');
+
 Route::resource('task', TaskController::class);
